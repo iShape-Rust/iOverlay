fc9cef8c497d5d8d61b6829dff490169006abc26
Nail Sharipov <nailxsharipov@gmail.com>
2023-12-25 10:31:17 +0300

    hello world test

--- tests/bool_tests.rs
+++ tests/bool_tests.rs
@@ -2,12 +2,42 @@ use i_overlay::bool::overlay_rule::OverlayRule;
 
 #[cfg(test)]
 mod tests {
+    use i_float::fix_vec::FixVec;
+    use i_shape::fix_shape::FixShape;
+    use i_overlay::bool::fill_rule::FillRule;
+    use i_overlay::layout::overlay::{Overlay, ShapeType};
     use super::*;
-    
+
     #[test]
     fn test_fill_rule() {
-        let rule = OverlayRule::Subject;
-        
-        assert_eq!(rule, OverlayRule::Subject);
+        let mut overlay = Overlay::new(2);
+
+        let left_bottom_square = FixShape::new_with_contour([
+            FixVec::new_f64(-10.0, -10.0),
+            FixVec::new_f64(-10.0, 10.0),
+            FixVec::new_f64(10.0, 10.0),
+            FixVec::new_f64(10.0, -10.0)
+        ].to_vec());
+
+        let right_top_square = FixShape::new_with_contour([
+            FixVec::new_f64(-5.0, -5.0),
+            FixVec::new_f64(-5.0, 15.0),
+            FixVec::new_f64(15.0, 15.0),
+            FixVec::new_f64(15.0, -5.0)
+        ].to_vec());
+
+        // add new geometry
+        overlay.add_shape(&left_bottom_square, ShapeType::Subject);
+        overlay.add_shape(&right_top_square, ShapeType::Clip);
+
+        // resolve shapes geometry
+        let graph = overlay.build_graph(FillRule::EvenOdd);
+
+        // apply union operation and get result (in our case it will be only one element)
+        let shapes = graph.extract_shapes(OverlayRule::Union);
+
+        // do something with new shapes...
+
+        print!("shapes: {:?}", shapes)
     }
 }
