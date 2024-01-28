0e43e4c3aa7d9f29e7e52c6692a18e5fd2c02722
Nail Sharipov <nailxsharipov@gmail.com>
2024-01-28 20:45:16 +0300

    fix top range

--- src/geom/x_scan_list.rs
+++ src/geom/x_scan_list.rs
@@ -3,30 +3,32 @@ use crate::space::scan_space::ScanSpace;
 
 pub(crate) struct XScanList {
     pub(crate) space: ScanSpace<usize, i32>,
-    bottom: i32,
     delta: i32,
 }
 
 impl XScanList {
     pub(crate) fn new(range: LineRange, count: usize) -> Self {
         let space = ScanSpace::new(range, count);
-        let bottom = range.min;
         let delta = 1 << space.indexer.scale;
-        Self { space, bottom, delta }
+        Self { space, delta }
     }
 
     pub(crate) fn iterator_to_bottom(&self, start: i32) -> LineRange {
-        let min_y = self.bottom.max(start - self.delta);
-        LineRange { min: min_y, max: start }
+        let range = self.space.indexer.range;
+        let top = range.max.min(start);
+        let min_y = range.min.max(top - self.delta);
+        LineRange { min: min_y, max: top }
     }
 
     pub(crate) fn next(&self, range: LineRange) -> LineRange {
-        if range.min > self.bottom {
+        let bottom = self.space.indexer.range.min;
+        if range.min > bottom {
             let radius = (range.max - range.min) << 1;
-            let min_y = self.bottom.max(range.min - radius);
+            let min_y = bottom.max(range.min - radius);
             return LineRange { min: min_y, max: range.min };
         } else {
             LineRange { min: i32::MIN, max: i32::MAX }
         }
     }
+
 }
