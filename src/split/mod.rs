7f465fa56a9c13dc6f125f8dc12ca0a5521e58eb
Nail Sharipov <nailxsharipov@gmail.com>
2024-03-28 09:56:24 +0300

    close access

--- src/split/mod.rs
+++ src/split/mod.rs
@@ -1,11 +1,11 @@
 pub mod shape_edge;
 pub mod shape_count;
 pub mod shape_edge_cross;
-pub(super) mod split_linked_list;
 pub(super) mod split_edges;
-mod version_index;
+mod split_linked_list;
 mod split_range_list;
-mod scan_store;
+mod version_index;
 mod version_segment;
+mod scan_store;
 mod scan_list;
 mod scan_tree;
--- src/split/version_index.rs
+++ src/split/version_index.rs
@@ -1,25 +1,25 @@
 use crate::index::EMPTY_INDEX;
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
-pub struct DualIndex {
-    pub major: usize,
-    pub minor: usize
+pub(super) struct DualIndex {
+    pub(super) major: usize,
+    pub(super) minor: usize,
 }
 
 impl DualIndex {
-    pub const EMPTY: Self = Self { major: usize::MAX, minor: usize::MAX };
+    pub(super) const EMPTY: Self = Self { major: usize::MAX, minor: usize::MAX };
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
-pub (super) struct VersionedIndex {
-    pub (super) version: usize,
-    pub (super) index: DualIndex
+pub(super) struct VersionedIndex {
+    pub(super) version: usize,
+    pub(super) index: DualIndex,
 }
 
 impl VersionedIndex {
-    pub (super) const EMPTY: Self = Self { version: usize::MAX, index: DualIndex::EMPTY };
+    pub(super) const EMPTY: Self = Self { version: usize::MAX, index: DualIndex::EMPTY };
 
-    pub (super) fn is_not_nil(&self) -> bool {
+    pub(super) fn is_not_nil(&self) -> bool {
         self.index.major != EMPTY_INDEX && self.index.minor != EMPTY_INDEX
     }
 }
