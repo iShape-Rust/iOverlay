a40426480730acb352cdff76fb5c3f32ec58dbf0
Nail Sharipov <nailxsharipov@gmail.com>
2024-04-22 22:29:40 +0300

    add min area

--- src/core/simplify.rs
+++ src/core/simplify.rs
@@ -5,46 +5,46 @@ use crate::core::overlay::{Overlay, ShapeType};
 use crate::core::overlay_rule::OverlayRule;
 
 pub trait Simplify {
-    fn simplify(&self, fill_rule: FillRule) -> Vec<IntShape>;
+    fn simplify(&self, fill_rule: FillRule, min_area: i64) -> Vec<IntShape>;
 }
 
 impl Simplify for IntPath {
-    fn simplify(&self, fill_rule: FillRule) -> Vec<IntShape> {
+    fn simplify(&self, fill_rule: FillRule, min_area: i64) -> Vec<IntShape> {
         let mut overlay = Overlay::new(self.len());
         overlay.add_path(self, ShapeType::Subject);
 
         let graph = overlay.build_graph(fill_rule);
-        graph.extract_shapes(OverlayRule::Subject)
+        graph.extract_shapes_min_area(OverlayRule::Subject, min_area)
     }
 }
 
 impl Simplify for [IntPath] {
-    fn simplify(&self, fill_rule: FillRule) -> Vec<IntShape> {
+    fn simplify(&self, fill_rule: FillRule, min_area: i64) -> Vec<IntShape> {
         let mut overlay = Overlay::new(self.len());
 
         overlay.add_paths(self, ShapeType::Subject);
 
         let graph = overlay.build_graph(fill_rule);
-        graph.extract_shapes(OverlayRule::Subject)
+        graph.extract_shapes_min_area(OverlayRule::Subject, min_area)
     }
 }
 
 impl Simplify for IntShape {
-    fn simplify(&self, fill_rule: FillRule) -> Vec<IntShape> {
+    fn simplify(&self, fill_rule: FillRule, min_area: i64) -> Vec<IntShape> {
         let mut overlay = Overlay::new(self[0].len());
         overlay.add_shape(self, ShapeType::Subject);
         let graph = overlay.build_graph(fill_rule);
-        graph.extract_shapes(OverlayRule::Subject)
+        graph.extract_shapes_min_area(OverlayRule::Subject, min_area)
     }
 }
 
 impl Simplify for [IntShape] {
-    fn simplify(&self, fill_rule: FillRule) -> Vec<IntShape> {
+    fn simplify(&self, fill_rule: FillRule, min_area: i64) -> Vec<IntShape> {
         let mut overlay = Overlay::new(self.len());
         for shape in self.iter() {
             overlay.add_shape(shape, ShapeType::Subject);
         }
         let graph = overlay.build_graph(fill_rule);
-        graph.extract_shapes(OverlayRule::Subject)
+        graph.extract_shapes_min_area(OverlayRule::Subject, min_area)
     }
 }
