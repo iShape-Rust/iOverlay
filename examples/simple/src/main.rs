4b2ac73871e10335f584426b93ff0a0229bfe0ea
Nail Sharipov <nailxsharipov@gmail.com>
2024-04-24 13:53:13 +0300

    update example

--- examples/simple/src/main.rs
+++ examples/simple/src/main.rs
@@ -1,28 +1,29 @@
-use i_float::{fix_float::FixMath, fix_vec::FixVec};
-use i_overlay::{
-    bool::{fill_rule::FillRule, overlay_rule::OverlayRule},
-    layout::overlay::{Overlay, ShapeType},
-};
+use i_float::f64_point::F64Point;
+use i_overlay::core::fill_rule::FillRule;
+use i_overlay::core::float_overlay::FloatOverlay;
+use i_overlay::core::overlay::ShapeType;
+use i_overlay::core::overlay_rule::OverlayRule;
 
 fn main() {
-    let mut overlay = Overlay::new(1);
 
     let subj = [
-        FixVec::new_number(-10, -10),
-        FixVec::new_number(-10, 10),
-        FixVec::new_number(10, 10),
-        FixVec::new_number(10, -10),
+        F64Point::new(-10.0, -10.0),
+        F64Point::new(-10.0, 10.0),
+        F64Point::new(10.0, 10.0),
+        F64Point::new(10.0, -10.0),
     ];
 
     let clip = [
-        FixVec::new_number(-5, -5),
-        FixVec::new_number(-5, 15),
-        FixVec::new_number(15, 15),
-        FixVec::new_number(15, -5),
+        F64Point::new(-5.0, -5.0),
+        F64Point::new(-5.0, 15.0),
+        F64Point::new(15.0, 15.0),
+        F64Point::new(15.0, -5.0),
     ];
 
-    overlay.add_path(&subj.to_vec(), ShapeType::Subject);
-    overlay.add_path(&clip.to_vec(), ShapeType::Clip);
+    let mut overlay = FloatOverlay::new();
+
+    overlay.add_path(&subj, ShapeType::Subject);
+    overlay.add_path(&clip, ShapeType::Clip);
     let graph = overlay.build_graph(FillRule::NonZero);
 
     let shapes = graph.extract_shapes(OverlayRule::Union);
@@ -30,11 +31,11 @@ fn main() {
     println!("shapes count: {}", shapes.len());
 
     if shapes.len() > 0 {
-        let contour = shapes[0].contour();
+        let contour = &shapes[0][0];
         println!("shape 0 contour: ");
         for p in contour {
-            let x = p.x.f32();
-            let y = p.x.f32();
+            let x = p.x;
+            let y = p.y;
             println!("({}, {})", x, y);
         }
     }
