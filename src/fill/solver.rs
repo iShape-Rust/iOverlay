4e72e41d25fdf90de3262154e21bf3695cef669d
Nail Sharipov <nailxsharipov@gmail.com>
2024-08-23 21:48:33 +0300

    simplify

--- src/fill/solver.rs
+++ src/fill/solver.rs
@@ -1,6 +1,6 @@
 use crate::core::fill_rule::FillRule;
 use crate::segm::shape_count::ShapeCount;
-use crate::segm::segment::{Segment, CLIP_BOTTOM, CLIP_TOP, NONE, SUBJ_BOTTOM, SUBJ_TOP, SegmentFill};
+use crate::segm::segment::{Segment, SegmentFill};
 
 pub(crate) struct FillSolver {}
 
@@ -17,36 +17,22 @@ impl FillSolver {
 impl Segment {
     #[inline]
     pub(super) fn add_and_fill(&self, sum_count: ShapeCount, fill_rule: FillRule) -> (ShapeCount, SegmentFill) {
-        let is_subj_top: bool;
-        let is_subj_bottom: bool;
-        let is_clip_top: bool;
-        let is_clip_bottom: bool;
-
         let new_count = sum_count.add(self.count);
 
-        match fill_rule {
+        let fill: SegmentFill = match fill_rule {
             FillRule::EvenOdd => {
-                is_subj_top = 1 & new_count.subj == 1;
-                is_subj_bottom = 1 & sum_count.subj == 1;
-
-                is_clip_top = 1 & new_count.clip == 1;
-                is_clip_bottom = 1 & sum_count.clip == 1;
+                (1 & new_count.subj as SegmentFill) // SUBJ_TOP
+                    | ((1 & sum_count.subj as SegmentFill) << 1) // SUBJ_BOTTOM
+                    | ((1 & new_count.clip as SegmentFill) << 2) // CLIP_TOP
+                    | ((1 & sum_count.clip as SegmentFill) << 3) // CLIP_BOTTOM
             }
             FillRule::NonZero => {
-                is_subj_top = new_count.subj != 0;
-                is_subj_bottom = sum_count.subj != 0;
-
-                is_clip_top = new_count.clip != 0;
-                is_clip_bottom = sum_count.clip != 0;
+                (new_count.subj != 0) as SegmentFill // SUBJ_TOP
+                    | ((sum_count.subj != 0) as SegmentFill) << 1 // SUBJ_BOTTOM
+                    | ((new_count.clip != 0) as SegmentFill) << 2 // CLIP_TOP
+                    | ((sum_count.clip != 0) as SegmentFill) << 3 // CLIP_BOTTOM
             }
-        }
-
-        let subj_top = if is_subj_top { SUBJ_TOP } else { NONE };
-        let subj_bottom = if is_subj_bottom { SUBJ_BOTTOM } else { NONE };
-        let clip_top = if is_clip_top { CLIP_TOP } else { NONE };
-        let clip_bottom = if is_clip_bottom { CLIP_BOTTOM } else { NONE };
-
-        let fill = subj_top | subj_bottom | clip_top | clip_bottom;
+        };
 
         (new_count, fill)
     }
