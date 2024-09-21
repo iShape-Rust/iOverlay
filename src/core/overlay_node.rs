23748e42d4a8310f6ce910cb5c4b1be0eb0a5249
Nail Sharipov <nailxsharipov@gmail.com>
2024-09-21 21:43:21 +0300

    improve

--- src/core/overlay_node.rs
+++ src/core/overlay_node.rs
@@ -7,14 +7,9 @@ impl OverlayNode {
     #[inline]
     pub(crate) fn other(&self, index: usize) -> usize {
         debug_assert_eq!(self.indices.len(), 2);
-        let (i0, i1) = unsafe {
-            let i0 = *self.indices.get_unchecked(0);
-            let i1 = *self.indices.get_unchecked(1);
-            (i0, i1)
-        };
-
+        let i0 = unsafe { *self.indices.get_unchecked(0) };
         if i0 == index {
-            i1
+            unsafe { *self.indices.get_unchecked(1) }
         } else {
             i0
         }
