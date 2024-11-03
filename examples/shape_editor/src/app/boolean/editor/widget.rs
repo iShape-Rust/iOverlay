a78a941b837c7e53f58585d0d802c2f09121bdbf
Nail Sharipov <nailxsharipov@gmail.com>
2024-11-03 21:23:13 +0300

    points mesh

--- examples/shape_editor/src/app/boolean/editor/widget.rs
+++ examples/shape_editor/src/app/boolean/editor/widget.rs
@@ -79,17 +79,9 @@ impl<Message> Widget<Message, Theme, Renderer> for SubjClipEditorWidget<'_, Mess
         _shell: &mut Shell<'_, Message>,
         viewport: &Rectangle,
     ) -> event::Status {
-        println!("on event {:?}", event);
-
-        let camera = if let State::Some(stete_box) = &mut tree.state {
+        if let State::Some(stete_box) = &mut tree.state {
             let state = stete_box.downcast_mut::<SubjClipEditorState>().unwrap();
-            if let Some(camera) = state.init_camera(&self.stateless.editor_points, viewport) {
-                camera
-            } else {
-                return event::Status::Ignored;
-            }
-        } else {
-            return event::Status::Ignored;
+            state.update_camera(&self.stateless.editor_points, viewport)
         };
 
         match event {
@@ -124,7 +116,7 @@ impl<Message> Widget<Message, Theme, Renderer> for SubjClipEditorWidget<'_, Mess
 
     fn draw(
         &self,
-        _tree: &Tree,
+        tree: &Tree,
         renderer: &mut Renderer,
         _theme: &Theme,
         _style: &renderer::Style,
@@ -132,105 +124,26 @@ impl<Message> Widget<Message, Theme, Renderer> for SubjClipEditorWidget<'_, Mess
         cursor: mouse::Cursor,
         _viewport: &Rectangle,
     ) {
-        // let camera = if let Some(camera) = &self.state.camera {
-        //     camera
-        // };
+        let state = if let State::Some(stete_box) = &tree.state {
+            stete_box.downcast_ref::<SubjClipEditorState>().unwrap()
+        } else {
+            return;
+        };
 
         use iced::advanced::graphics::mesh::{
             self, Mesh, Renderer as _, SolidVertex2D,
         };
         use iced::advanced::Renderer as _;
 
-        let bounds = layout.bounds();
-        println!("bounds: {:?}", bounds);
-        // R O Y G B I V
-        let color_r = [1.0, 0.0, 0.0, 1.0];
-        let color_o = [1.0, 0.5, 0.0, 1.0];
-        let color_y = [1.0, 1.0, 0.0, 1.0];
-        let color_g = [0.0, 1.0, 0.0, 1.0];
-        let color_gb = [0.0, 1.0, 0.5, 1.0];
-        let color_b = [0.0, 0.2, 1.0, 1.0];
-        let color_i = [0.5, 0.0, 1.0, 1.0];
-        let color_v = [0.75, 0.0, 0.5, 1.0];
-
-        let posn_center = {
-            if let Some(cursor_position) = cursor.position_in(bounds) {
-                [cursor_position.x, cursor_position.y]
-            } else {
-                [bounds.width / 2.0, bounds.height / 2.0]
-            }
-        };
-
-        let posn_tl = [0.0, 0.0];
-        let posn_t = [bounds.width / 2.0, 0.0];
-        let posn_tr = [bounds.width, 0.0];
-        let posn_r = [bounds.width, bounds.height / 2.0];
-        let posn_br = [bounds.width, bounds.height];
-        let posn_b = [(bounds.width / 2.0), bounds.height];
-        let posn_bl = [0.0, bounds.height];
-        let posn_l = [0.0, bounds.height / 2.0];
-
-        let mesh = Mesh::Solid {
-            buffers: mesh::Indexed {
-                vertices: vec![
-                    SolidVertex2D {
-                        position: posn_center,
-                        color: color::pack([1.0, 1.0, 1.0, 1.0]),
-                    },
-                    SolidVertex2D {
-                        position: posn_tl,
-                        color: color::pack(color_r),
-                    },
-                    SolidVertex2D {
-                        position: posn_t,
-                        color: color::pack(color_o),
-                    },
-                    SolidVertex2D {
-                        position: posn_tr,
-                        color: color::pack(color_y),
-                    },
-                    SolidVertex2D {
-                        position: posn_r,
-                        color: color::pack(color_g),
-                    },
-                    SolidVertex2D {
-                        position: posn_br,
-                        color: color::pack(color_gb),
-                    },
-                    SolidVertex2D {
-                        position: posn_b,
-                        color: color::pack(color_b),
-                    },
-                    SolidVertex2D {
-                        position: posn_bl,
-                        color: color::pack(color_i),
-                    },
-                    SolidVertex2D {
-                        position: posn_l,
-                        color: color::pack(color_v),
-                    },
-                ],
-                indices: vec![
-                    0, 1, 2, // TL
-                    0, 2, 3, // T
-                    0, 3, 4, // TR
-                    0, 4, 5, // R
-                    0, 5, 6, // BR
-                    0, 6, 7, // B
-                    0, 7, 8, // BL
-                    0, 8, 1, // L
-                ],
-            },
-            transformation: Transformation::IDENTITY,
-            clip_bounds: Rectangle::INFINITE,
-        };
-
-        renderer.with_translation(
-            Vector::new(bounds.x, bounds.y),
-            |renderer| {
-                renderer.draw_mesh(mesh);
-            },
-        );
+        if let Some(mesh) = state.point_mesh(&self.stateless.editor_points) {
+            let bounds = layout.bounds();
+            renderer.with_translation(
+                Vector::new(bounds.x, bounds.y),
+                |renderer| {
+                    renderer.draw_mesh(mesh);
+                },
+            );
+        }
     }
 }
 
