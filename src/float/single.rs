0dd0ce9c3e4e348d6e63a23b207e0941649f75c0
Nail Sharipov <nailxsharipov@gmail.com>
2025-02-27 16:25:52 +0300

    add api method

--- src/float/single.rs
+++ src/float/single.rs
@@ -3,6 +3,9 @@ use i_float::float::number::FloatNumber;
 use i_shape::base::data::Shapes;
 use crate::core::fill_rule::FillRule;
 use crate::core::overlay_rule::OverlayRule;
+use crate::core::solver::Solver;
+use crate::float::filter;
+use crate::float::filter::ContourFilter;
 use crate::float::overlay::FloatOverlay;
 use crate::float::source::resource::OverlayResource;
 
@@ -26,6 +29,22 @@ where
     /// - `fill_rule`: Fill rule to determine filled areas (non-zero, even-odd, positive, negative).
     /// - Returns: A vector of `Shapes<P>` representing the cleaned-up geometric result.
     fn overlay(&self, source: &R1, overlay_rule: OverlayRule, fill_rule: FillRule) -> Shapes<P>;
+
+    /// General overlay method that takes an `OverlayResource` to determine the input type.
+    ///
+    /// - `resource`: A `OverlayResource` specifying the type of geometric entity to overlay with.
+    ///   It can be one of the following:
+    ///     - `Contour`: A single contour representing a path or boundary.
+    ///     - `Contours`: A collection of contours, each defining separate boundaries.
+    ///     - `Shapes`: A collection of shapes, where each shape may consist of multiple contours.
+    /// - `overlay_rule`: The boolean operation rule to apply when extracting shapes from the graph, such as union or intersection.
+    /// - `fill_rule`: Fill rule to determine filled areas (non-zero, even-odd, positive, negative).
+    /// - `filter`: `ContourFilter<T>` for optional contour filtering and simplification:
+    ///     - `min_area`: Only retain contours with an area larger than this.
+    ///     - `simplify`: Simplifies contours and removes degenerate edges if `true`.
+    /// - `solver`: Type of solver to use.
+    /// - Returns: A vector of `Shapes<P>` representing the cleaned-up geometric result.
+    fn overlay_with_filter_and_solver(&self, source: &R1, overlay_rule: OverlayRule, fill_rule: FillRule, filter: ContourFilter<T>, solver: Solver) -> Shapes<P>;
 }
 
 impl<R0, R1, P, T> SingleFloatOverlay<R0, R1, P, T> for R0
@@ -37,7 +56,18 @@ where
 {
     #[inline]
     fn overlay(&self, resource: &R1, overlay_rule: OverlayRule, fill_rule: FillRule) -> Shapes<P> {
-        FloatOverlay::with_subj_and_clip(self, resource).overlay(overlay_rule, fill_rule)
+        self.overlay_with_filter_and_solver(
+            resource,
+            overlay_rule,
+            fill_rule,
+            Default::default(),
+            Default::default()
+        )
+    }
+
+    #[inline]
+    fn overlay_with_filter_and_solver(&self, resource: &R1, overlay_rule: OverlayRule, fill_rule: FillRule, filter: ContourFilter<T>, solver: Solver) -> Shapes<P> {
+        FloatOverlay::with_subj_and_clip(self, resource).overlay_with_filter_and_solver(overlay_rule, fill_rule, filter, solver)
     }
 }
 
