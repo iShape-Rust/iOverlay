6538c1ac62f0116fd7311cb2a947e441293bc96e
Nail Sharipov <nailxsharipov@gmail.com>
2026-02-08 21:23:51 +0300

    add more tests

--- iOverlay/tests/ocg_tests.rs
+++ iOverlay/tests/ocg_tests.rs
@@ -148,6 +148,7 @@ mod tests {
         //   1 │   └───┘   │
         //     │           │
         //   0 └───────────┘
+
         let subj_paths = int_shape![[[0, 3], [0, 0], [3, 0], [3, 2], [1, 2], [1, 1], [2, 1], [2, 3]]];
 
         let clip_paths = int_shape![];
@@ -179,6 +180,7 @@ mod tests {
         //   1 │   └───┘       │
         //     │               │
         //   0 └───────────────┘
+
         let subj_paths = int_shape![
             [[0, 4], [0, 0], [4, 0], [4, 3], [3, 3], [3, 4]]
         ];
@@ -206,6 +208,44 @@ mod tests {
 
     #[test]
     fn test_5() {
+        //     0   1   2   3   4
+        //   4 ┌───────────────┐
+        //     │               │
+        //   3 │       ┌───┐   │
+        //     │       │ ░ │   │
+        //   2 │   ┌───●───┘   │
+        //     │   │ ░ │       │
+        //   1 │   └───┘       │
+        //     │               │
+        //   0 └───────────────┘
+
+        let subj_paths = int_shape![
+            [[0, 4], [0, 0], [4, 0], [4, 4]]
+        ];
+
+        let clip_paths = int_shape![
+            [[1, 2], [1, 1], [2, 1], [2, 2]],
+            [[2, 3], [2, 2], [3, 2], [3, 3]],
+        ];
+
+        let mut overlay = Overlay::with_contours_custom(
+            &subj_paths,
+            &clip_paths,
+            IntOverlayOptions::ogc(),
+            Default::default(),
+        );
+
+        let result = overlay.overlay(OverlayRule::Difference, FillRule::EvenOdd);
+
+        assert_eq!(result.len(), 1);
+        assert_eq!(result[0].len(), 3);
+        assert_eq!(result[0][0].len(), 4);
+        assert_eq!(result[0][1].len(), 4);
+        assert_eq!(result[0][2].len(), 4);
+    }
+
+    #[test]
+    fn test_6() {
         //     0   1   2   3
         //   3     ┌───┐
         //         │   │
@@ -245,7 +285,7 @@ mod tests {
     }
 
     #[test]
-    fn test_6() {
+    fn test_7() {
         let subj_paths = int_shape![
             [[-3, 0], [-3, -3], [0, -3], [0, 0], [3, 0], [3, 3], [0, 3], [0, 0]],
             [[-1, -2], [-2, -1], [0, 0], [1, 2], [2, 1], [0, 0]],
